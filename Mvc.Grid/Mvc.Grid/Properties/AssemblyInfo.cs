8185a8af5b3dfa56c6e9058a6ff168f4a0b44af2
Muchiachio <aldis.rameika@gmail.com>
2014-10-29 09:56:53 +0200

    Initial commit
    - Initial commit.

---
+++ Mvc.Grid/Mvc.Grid/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+
+[assembly: ComVisible(false)]
+[assembly: AssemblyTitle("Mvc.Grid")]
+[assembly: AssemblyProduct("Mvc.Grid")]
+[assembly: AssemblyCompany("NonFactors")]
+[assembly: AssemblyCopyright("Copyright © NonFactors")]
+[assembly: NeutralResourcesLanguage("en")]
+[assembly: AssemblyFileVersion("0.0.1.0")]
+[assembly: AssemblyVersion("0.0.1.0")]
