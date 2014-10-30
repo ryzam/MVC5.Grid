7291c8702e14690eec530cd170deb32f24ccddf3
Muchiachio <aldis.rameika@gmail.com>
2014-10-30 09:28:29 +0200

    Partial view rendering
    - Added html helper extension for rendering html grids from custom
    partial views.
    - Added source and test solution folders.

--- src/Mvc.Grid/Html/GridHtmlExtensions.cs
+++ src/Mvc.Grid/Html/GridHtmlExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Web.Mvc;
 
@@ -9,6 +10,13 @@ namespace NonFactors.Mvc.Grid.Html
         public static HtmlGrid<TModel> Grid<TModel>(this HtmlHelper html, IEnumerable<TModel> source) where TModel : class
         {
             return new HtmlGrid<TModel>(html, new Grid<TModel>(source.AsQueryable()));
+        }
+        public static HtmlGrid<TModel> Grid<TModel>(this HtmlHelper html, String partialViewName, IEnumerable<TModel> source) where TModel : class
+        {
+            HtmlGrid<TModel> grid = new HtmlGrid<TModel>(html, new Grid<TModel>(source.AsQueryable()));
+            grid.PartialViewName = partialViewName;
+
+            return grid;
         }
     }
 }
