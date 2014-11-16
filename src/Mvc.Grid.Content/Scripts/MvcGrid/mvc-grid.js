208ad06fa7e9b07e9a12924605238fd7b8cbb639
Muchiachio <aldis.rameika@gmail.com>
2014-11-16 21:05:06 +0200

    Grid filter base
    - Added initial filter widget base for client side.

--- src/Mvc.Grid.Content/Scripts/MvcGrid/mvc-grid.js
+++ src/Mvc.Grid.Content/Scripts/MvcGrid/mvc-grid.js
@@ -17,12 +17,57 @@
     MvcGrid.prototype = {
         init: function () {
             this.initSorting();
+            this.initFiltering();
         },
 
         initSorting: function () {
             this.grid.find("th[data-sort='True']").on('click.mvcgrid', function () {
                 window.location.href = $(this).data('sort-query');
             });
+        },
+
+        initFiltering: function () {
+            var that = this;
+            this.grid.find("th[data-filter='True'] .mvc-grid-filter").on('click.mvcgrid', function (e) {
+                e.stopPropagation();
+                e.preventDefault();
+
+                var filterType = $(this).parent().data('filter-type');
+                if ($.fn.mvcgrid.filters[filterType]) {
+                    var filterPopup = $('body').children('.mvc-grid-filter-popup');
+                    if (filterPopup.length > 0) {
+                        filterPopup.addClass("open");
+                        filterPopup.html($.fn.mvcgrid.filters[filterType]("Equals"));
+                    } else {
+                        $('body').append("<div class='mvc-grid-filter-popup dropdown-menu open'><div class='popup-arrow'></div>" + $.fn.mvcgrid.filters[filterType]("Equals") + "</div>");
+                        filterPopup = $('body').children('.mvc-grid-filter-popup');
+                    }
+
+                    that.setFilterPopupPosition($(this), filterPopup);
+                }
+            });
+        },
+        setFilterPopupPosition: function (filter, popup) {
+            var arrow = popup.find(".popup-arrow");
+
+            var dropdownWidth = popup.width();
+            var popupLeft = filter.offset().left;
+            var popupTop = filter.offset().top;
+            var winWidth = $(window).width();
+            var dropdownTop = popupTop + 20;
+            var dropdownLeft = 0;
+            var arrowLeft = 0;
+
+            if (popupLeft + dropdownWidth + 10 > winWidth) {
+                dropdownLeft = winWidth - dropdownWidth - 20;
+                arrowLeft = popupLeft - dropdownLeft - 3;
+            } else {
+                dropdownLeft = popupLeft - 30;
+                arrowLeft = 17;
+            }
+
+            popup.attr("style", "display: block; left: " + dropdownLeft + "px; top: " + dropdownTop + "px !important");
+            arrow.css("left", arrowLeft + "px");
         }
     };
 
@@ -32,6 +77,23 @@
                 $.data(this, "mvc-grid", new MvcGrid(this));
             }
         });
+    };
+
+    $.fn.mvcgrid.filters = {
+        Text: function (value) {
+            return (
+                '<div class="form-group">' +
+                    '<select class="form-control">' +
+                        '<option value="Equals" ' + (value == "Equals" ? "selected=\"selected\"" : "") + '>Equals</option>' +
+                    '</select>' +
+                '</div>' +
+                '<div class="form-group">' +
+                    '<input class="form-control mvc-grid-input" type="text">' +
+                '</div>' +
+                '<div class="mvc-grid-filter-buttons">' +
+                    '<button class="btn btn-primary btn-block" type="button">Apply</button>' +
+                '</div>');
+        }
     };
 
     $('.mvc-grid').mvcgrid();
