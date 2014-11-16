0a7bfbe5fde43f37f930da2e1cbeab2da8d01aab
Muchiachio <aldis.rameika@gmail.com>
2014-11-16 08:21:08 +0200

    Grid java script
    - Implemented initial java script code for mvc grid.

--- src/Mvc.Grid.Content/Scripts/MvcGrid/mvc-grid.js
+++ src/Mvc.Grid.Content/Scripts/MvcGrid/mvc-grid.js
@@ -1,5 +1,5 @@
 ﻿/*!
- * Mvc.Grid 0.3.0
+ * Mvc.Grid 0.4.0
  * https://github.com/NonFactors/MVC.Grid
  *
  * Copyright © 2014 NonFactors
@@ -8,6 +8,31 @@
  * http://www.opensource.org/licenses/mit-license.php
  */
 
-$(".mvc-grid-table th[data-sort='True']").on('click', function () {
-    window.location.href = $(this).data('sort-query');
-});
+(function ($) {
+    function MvcGrid(element) {
+        this.grid = $(element);
+        this.init();
+    }
+
+    MvcGrid.prototype = {
+        init: function () {
+            this.initSorting();
+        },
+
+        initSorting: function () {
+            this.grid.find("th[data-sort='True']").on('click.mvcgrid', function () {
+                window.location.href = $(this).data('sort-query');
+            });
+        }
+    };
+
+    $.fn.mvcgrid = function () {
+        return this.each(function () {
+            if (!$.data(this, "mvc-grid")) {
+                $.data(this, "mvc-grid", new MvcGrid(this));
+            }
+        });
+    };
+
+    $('.mvc-grid').mvcgrid();
+})(jQuery);
