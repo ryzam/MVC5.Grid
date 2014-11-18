8231bc259f4b5942bbd4e9c27a384444e2866cc7
Muchiachio <aldis.rameika@gmail.com>
2014-11-18 17:38:42 +0200

    Rendering grid filter value
    - Added current grid filter value rendering in grid filter.

--- src/Mvc.Grid.Content/Scripts/MvcGrid/mvc-grid.js
+++ src/Mvc.Grid.Content/Scripts/MvcGrid/mvc-grid.js
@@ -21,34 +21,43 @@
         },
 
         initSorting: function () {
-            this.grid.find("th[data-sort='True']").on('click.mvcgrid', function () {
+            this.grid.find('th[data-sort="True"]').on('click.mvcgrid', function () {
                 window.location.href = $(this).data('sort-query');
             });
         },
 
         initFiltering: function () {
+            this.createFilterPopup();
+            this.bindFilters();
+        },
+        createFilterPopup: function () {
+            if ($('body').children('.mvc-grid-filter-popup').length == 0) {
+                $('body').append('<div class="mvc-grid-filter-popup dropdown-menu">' +
+                                    '<div class="popup-arrow"></div>' +
+                                    '<div class="popup-content"></div>' +
+                                 '</div>');
+            }
+        },
+        bindFilters: function () {
             var that = this;
-            this.grid.find("th[data-filter='True'] .mvc-grid-filter").on('click.mvcgrid', function (e) {
+
+            this.grid.find('th[data-filter="True"] .mvc-grid-filter').on('click.mvcgrid', function (e) {
                 e.stopPropagation();
                 e.preventDefault();
 
                 var filterName = $(this).parent().data('filter-name');
+                var filterValue = $(this).parent().data('filter-value');
                 if ($.fn.mvcgrid.filters[filterName]) {
                     var filterPopup = $('body').children('.mvc-grid-filter-popup');
-                    if (filterPopup.length > 0) {
-                        filterPopup.addClass("open");
-                        filterPopup.html($.fn.mvcgrid.filters[filterName]("Equals"));
-                    } else {
-                        $('body').append("<div class='mvc-grid-filter-popup dropdown-menu open'><div class='popup-arrow'></div>" + $.fn.mvcgrid.filters[filterType]("Equals") + "</div>");
-                        filterPopup = $('body').children('.mvc-grid-filter-popup');
-                    }
+                    filterPopup.find('.popup-content').html($.fn.mvcgrid.filters[filterName].render(filterValue));
+                    filterPopup.addClass('open');
 
                     that.setFilterPopupPosition($(this), filterPopup);
                 }
             });
         },
         setFilterPopupPosition: function (filter, popup) {
-            var arrow = popup.find(".popup-arrow");
+            var arrow = popup.find('.popup-arrow');
 
             var dropdownWidth = popup.width();
             var popupLeft = filter.offset().left;
@@ -66,34 +75,41 @@
                 arrowLeft = 17;
             }
 
-            popup.attr("style", "display: block; left: " + dropdownLeft + "px; top: " + dropdownTop + "px !important");
-            arrow.css("left", arrowLeft + "px");
+            popup.attr('style', 'display: block; left: ' + dropdownLeft + 'px; top: ' + dropdownTop + 'px !important');
+            arrow.css('left', arrowLeft + 'px');
         }
     };
 
     $.fn.mvcgrid = function () {
         return this.each(function () {
-            if (!$.data(this, "mvc-grid")) {
-                $.data(this, "mvc-grid", new MvcGrid(this));
+            if (!$.data(this, 'mvc-grid')) {
+                $.data(this, 'mvc-grid', new MvcGrid(this));
             }
         });
     };
 
     $.fn.mvcgrid.filters = {
-        Text: function (value) {
-            return (
-                '<div class="form-group">' +
-                    '<select class="form-control">' +
-                        '<option value="Equals" ' + (value == "Equals" ? "selected=\"selected\"" : "") + '>Equals</option>' +
-                    '</select>' +
-                '</div>' +
-                '<div class="form-group">' +
-                    '<input class="form-control mvc-grid-input" type="text">' +
-                '</div>' +
-                '<div class="mvc-grid-filter-buttons">' +
-                    '<button class="btn btn-primary btn-block" type="button">Apply</button>' +
-                '</div>');
-        }
+        Text: (function ($) {
+            function GridTextFilter() {
+            }
+
+            GridTextFilter.prototype.render = function (value) {
+                return (
+                    '<div class="form-group">' +
+                        '<select class="form-control">' +
+                            '<option value="Equals" selected="selected">Equals</option>' +
+                        '</select>' +
+                    '</div>' +
+                    '<div class="form-group">' +
+                        '<input class="form-control mvc-grid-input" type="text" value="' + value + '">' +
+                    '</div>' +
+                    '<div class="mvc-grid-filter-buttons">' +
+                        '<button class="btn btn-primary btn-block mvc-grid-filter-apply" type="button">Apply</button>' +
+                    '</div>');
+            }
+
+            return new GridTextFilter();
+        })(jQuery)
     };
 
     $('.mvc-grid').mvcgrid();
