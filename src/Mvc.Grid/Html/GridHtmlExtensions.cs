ecf7eeab859829153ad2a36b658860a197fe6204
Muchiachio <aldis.rameika@gmail.com>
2015-01-02 19:43:29 +0200

    Html namespace removal
    - Removed unnecessary html namespace.

--- src/Mvc.Grid/Html/GridHtmlExtensions.cs
+++ src/Mvc.Grid/Html/GridHtmlExtensions.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Web.Mvc;
 
-namespace NonFactors.Mvc.Grid.Html
+namespace NonFactors.Mvc.Grid
 {
     public static class GridHtmlExtensions
     {
--- test/Mvc.Grid.Tests/Unit/Html/GridHtmlExtensionsTests.cs
+++ test/Mvc.Grid.Tests/Unit/Html/GridHtmlExtensionsTests.cs
@@ -1,5 +1,4 @@
-﻿using NonFactors.Mvc.Grid.Html;
-using NUnit.Framework;
+﻿using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
